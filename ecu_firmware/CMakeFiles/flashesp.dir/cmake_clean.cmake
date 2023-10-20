7711e4a02e449a43c29cf07f1d5e5b2f596f91b3
Zychlix <mzflighter@gmail.com>
2023-10-20 12:24:50 +0200

    Latest fixes

---
+++ ecu_firmware/CMakeFiles/flashesp.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flashesp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flashesp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ecu_firmware/miagl/CMakeFiles/miaui.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for miaui.
+# This may be replaced when dependencies are built.
